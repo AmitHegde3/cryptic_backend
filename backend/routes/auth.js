894a6e2c85db74a25a816ff239da1b5930dfa1cd
Amit Hegde <amit.is21@bmsce.ac.in>
2024-04-06 23:06:37 +0530

    Bug fix in /api/auth/createUser API endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User"); // Importing the User model
-const { query, validationResult } = require("express-validator"); // Importing validation utilities
+const { body, validationResult } = require("express-validator"); // Importing validation utilities
 const bcrypt = require("bcryptjs"); // Importing bcrypt for password hashing
 const jwt = require("jsonwebtoken"); // Importing JWT for token generation
-const fetchuser = require("../middleware/fetchuser") // Importing middleware for fetching user
+const fetchuser = require("../middleware/fetchuser")// Importing middleware for fetching user
 
-const JWT_SECRET = "This is a test"
-console.log("Secret toke is:",JWT_SECRET)
+const JWT_SECRET = "This is a test";
+// const JWT_SECRET = process.env.JWT_KEY; 
+
+console.log("Secret toke is:",JWT_SECRET) 
 router.use(express.json()); 
 
-// Router 1: Create a User using : POST "/api/auth/createUSer". Doesnt require auth
+// Router 1: Create a User using : POST "/api/auth/createUser". Doesnt require auth...Create authToken
 
 router.post("/createUser",
     [
         // Validation checks for name, email, and password
-        body("name", "Enter a valid Name").isLength({ min: 5 }),
-        query("email", "Enter a valid Email").isEmail(),
-        query("password", "Enter a valid Password").isLength({ min: 5 }),
-        query("age", "Enter a valid Age").isInt({ min: 18 }),
+        body("name", "Enter a valid Name").isLength({ min: 3 }),
+        body("email", "Enter a valid Email").isEmail(),
+        body("password", "Enter a valid Password").isLength({ min: 5 }),
+        body("age", "Enter a valid Age").isInt({ min: 18 }),
     ],
     async (req, res) => {
-        console.log(req.body);
+        // console.log(req.body); 
         const error = validationResult(req);
-
+ 
         if (!error.isEmpty()) {
-            console.log("Here")
-            res.send({ error, errors: error.array() });
+            res.status(400).send({ error, errors: error.array() });
             return;
         }
 
@@ -46,7 +47,7 @@ router.post("/createUser",
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                passowrd: secPass,
+                password: secPass,
                 age: req.body.age
             });
 
