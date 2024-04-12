732d6fd9702f28368a2b47ed807c5d9a1638b968
Amit Hegde <amit.is21@bmsce.ac.in>
2024-04-12 11:22:29 +0530

    Update Sell Coin functionalties

--- backend/routes/coins.js
+++ backend/routes/coins.js
@@ -90,5 +90,27 @@ router.put("/updatecoin/:id", fetchuser, async (req, res) => {
     }
   });
   
+// Route 4: Sell an existing Coin using : DELETE "/api/coins/sellcoin" . Login required
+router.delete("/sellcoin/:id", fetchuser, async (req, res) => {
+  try {
+    // const { title, price } = req.body;
+   
+    // Find the note to be deleted and delete it
+    let coin = await Coin.findById(req.params.id);
+    if (!coin) {
+      return res.status(404).send("Not Found");
+    }
+    // Allow deletion only if user owns this Coin
+    if (coin.user.toString() !== req.user.id) {
+      return res.status(401).send("Not allowed");
+    }
+
+    coin = await Coin.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Coin has been Sold", coin: coin });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error!");
+  }
+});
 
 module.exports = router;
